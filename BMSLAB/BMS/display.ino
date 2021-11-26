3bb3696505268f81927abb15986c1df222aa9d7b
tipafz02 <timilino@live.de>
2021-11-26 13:25:22 +0100

    Leerzeichen

--- BMSLAB/BMS/display.ino
+++ BMSLAB/BMS/display.ino
@@ -11,13 +11,13 @@ void showMeasurementValues()
   // write Cell Voltages on screen
   for (i = 1; i <= 4; i++)
   {
-    writeText(i*68-52, 4, 1, "Cell "+String(i), ILI9341_WHITE); //name und schriftgröße
-    writeText(i*68-52, 22, 1, String(getCellVoltage(i),3)+"V", colCell(i)); //schriftgröße position und einheit
-    writeText(i*68-52, 40, 1, strLen(String(getCellTemp(i),1),4)+"C", colTemp(i)); //schriftgröße position und einheit
+    writeText(i*68-52, 4, 2, "V"+String(i), ILI9341_WHITE);
+    writeText(i*68-66, 22, 1, strLen(String(getCellVoltage(i),3),6), colCell(i));     // Nachkommastellen bearbeitet auf 3
+    writeText(i*68-66, 40, 1, strLen(String(getCellTemp(i),2),5), colTemp(i));        // Nachkommastellen bearbeitet auf 2
   }
   // write pack current on screen
-  writeText(280,  4, 1, "I/A",ILI9341_LIGHTGREY); //neuer Name
-  writeText(280, 22, 1, strLen(String(getPackCurrent(),1),4),ILI9341_BLUE); //neue Schriftgröße Position
+  writeText(280,  4, 2, "I/A",ILI9341_LIGHTGREY);
+  writeText(280, 22, 1, strLen(String(getPackCurrent(),1),5),ILI9341_BLUE);           // Nachkommastellen bearbeitet auf 1
 }
 
 void drawMeasurementCurves(uint16_t fullScreenDuration)
